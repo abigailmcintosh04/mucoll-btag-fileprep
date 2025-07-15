9873236198a6c2001f558a63f2afd0802e88989b
Abigail McIntosh <user313@epldt069.ph.bham.ac.uk>
2025-07-15 09:57:20 +0100

    updated all schema

--- ucbtagfileprep/schema.py
+++ ucbtagfileprep/schema.py
@@ -19,4 +19,10 @@ dtype_consts = np.dtype([
     ("charge", np.int32),
     ("d0", np.float32),
     ('eta', np.float32),
+    ('phi', np.float32),
+    ('rel_eta', np.float32),
+    ('rel_phi', np.float32),
+    ('frac_pt', np.float32),
+    ('dr', np.float32),
+    ('z0', np.float32),
 ])
